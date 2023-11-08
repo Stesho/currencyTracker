691e87139c1b309c5fe78f9489652fc6e50bf304
Steshenko Dmitry <3301297@gmail.com>
2023-11-08 16:41:44 +0300

    feat: add chart with mock data

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,19 +1,37 @@
 import React, { PureComponent } from 'react';
 import { CurrencyRated } from '@/constants/interfaces/currency';
 import DropDown from '@/components/ui/dropdown/DropDown';
+import CurrencyCodeCard from '@/components/ui/currencyCodeCard/CurrencyCodeCard';
+import PriceChart from '@/components/priceChart/PriceChart';
 
 interface TimelineProps {
   currencies: CurrencyRated[];
 }
-interface TimelineState {}
+interface TimelineState {
+  selectedCurrency: CurrencyRated;
+}
 
 class Timeline extends PureComponent<TimelineProps, TimelineState> {
+  constructor(props: TimelineProps) {
+    super(props);
+    this.state = {
+      selectedCurrency: props.currencies[0],
+    };
+  }
+
   render() {
+    const { selectedCurrency } = this.state;
     const { currencies } = this.props;
 
     return (
       <div className='container'>
         <DropDown options={currencies} onSelectOption={() => {}} />
+        <CurrencyCodeCard
+          id={selectedCurrency.id}
+          iconUrl={selectedCurrency.iconUrl}
+          currencyName={selectedCurrency.currencyName}
+        />
+        <PriceChart />
       </div>
     );
   }

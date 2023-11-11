edfc7d73228875879b3b4da07f25b5294984ca62
Steshenko Dmitry <3301297@gmail.com>
2023-11-11 21:38:28 +0300

    feat: add bank card page

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,22 @@ import { Footer } from '@/components/ui/footer/Footer';
 import { Header } from '@/components/ui/header/Header';
 import { Info } from '@/components/ui/info/Info';
 import { TimeUpdate } from '@/components/ui/timeUpdate/TimeUpdate';
+import BankCardPage from '@/pages/bankCard/BankCardPage';
 import { HomePage } from '@/pages/home/HomePage';
 import { TimelinePage } from '@/pages/timeline/TimelinePage';
 
 export function App() {
-  return <BrowserRouter>
-    <Header />
-    <Info />
-    <TimeUpdate />
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='timeline' element={<TimelinePage />} />
-    </Routes>
-    <Footer />
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Info />
+      <TimeUpdate />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='timeline' element={<TimelinePage />} />
+        <Route path='bank-card' element={<BankCardPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/bankCard/BankCardPage.tsx
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+import { Input } from '@/components/ui/input/Input';
+
+class BankCardPage extends PureComponent {
+  render() {
+    return (
+      <main>
+        <Input />
+      </main>
+    );
+  }
+}
+
+export default BankCardPage;

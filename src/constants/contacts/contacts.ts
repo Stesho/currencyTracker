26ef3e8076405342aff2fbdfaf1f1f779916108d
Steshenko Dmitry <3301297@gmail.com>
2023-11-16 14:24:38 +0300

    feat: add adaptive design for contacts page

---
+++ src/constants/contacts/contacts.ts
@@ -0,0 +1,33 @@
+import FacebookIcon from '@/assets/icons/facebook.svg';
+import InstagramIcon from '@/assets/icons/instagram.svg';
+import LinkedInIcon from '@/assets/icons/linkedin.svg';
+import TwitterIcon from '@/assets/icons/twitter-x.svg';
+import YoutubeIcon from '@/assets/icons/youtube.svg';
+
+export const contacts = [
+  {
+    social: 'Instagram',
+    link: 'https://www.instagram.com/modsencompany',
+    icon: InstagramIcon,
+  },
+  {
+    social: 'Twitter',
+    link: 'https://twitter.com/modsencompany',
+    icon: TwitterIcon,
+  },
+  {
+    social: 'LinkedIn',
+    link: 'https://www.linkedin.com/company/modsen',
+    icon: LinkedInIcon,
+  },
+  {
+    social: 'Facebook',
+    link: 'https://www.facebook.com/ModsenSoftware',
+    icon: FacebookIcon,
+  },
+  {
+    social: 'Youtube',
+    link: 'https://www.youtube.com/@modsencompany',
+    icon: YoutubeIcon,
+  },
+];
